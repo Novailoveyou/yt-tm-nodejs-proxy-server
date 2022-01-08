56ec83f75335dcd2ffadd78a3f1e12c6b5856779
novailoveyou <novailoveyoux@gmail.com>
2022-01-08 20:51:28 +0300

    request from the server

---
+++ config/env.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
+module.exports = { API_BASE_URL, API_KEY_NAME, API_KEY_VALUE }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const { API_BASE_URL, API_KEY_NAME, API_KEY_VALUE } = require('../config/env')
 
-router.get('/', (req, res) => {
-  res.json({ success: true })
+router.get('/', async (req, res) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE
+    })
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+    const data = apiRes.body
+
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
 })
 
 module.exports = router
