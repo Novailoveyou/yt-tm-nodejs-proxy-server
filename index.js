95b983ee28d6bd38c1a8708458aacc22aed6bb2e
novailoveyou <novailoveyoux@gmail.com>
2022-01-09 01:38:28 +0300

    use server with client app

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const app = express()
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 Mins
-  max: 5
+  max: 100
 })
 
 app.use(limiter)
 app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public'))
 
 // Routes
 app.use('/api', require('./routes'))
