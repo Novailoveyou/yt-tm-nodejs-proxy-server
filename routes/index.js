29bd4d542eb87d476f1034a638a485caa25c152e
novailoveyou <novailoveyoux@gmail.com>
2022-01-09 01:28:37 +0300

    forwarding query params

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
@@ -6,11 +7,16 @@ const { API_BASE_URL, API_KEY_NAME, API_KEY_VALUE } = require('../config/env')
 router.get('/', async (req, res) => {
   try {
     const params = new URLSearchParams({
-      [API_KEY_NAME]: API_KEY_VALUE
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
     })
 
     const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
     const data = apiRes.body
+
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
 
     res.status(200).json(data)
   } catch (error) {
